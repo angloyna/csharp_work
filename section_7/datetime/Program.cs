3322af4830aeac70dde1e1cd181a6219b37cf4d9
Angela Gloyna <angela.gloyna@gmail.com>
2020-08-29 17:11:25 -0500

    finish section 7

---
+++ section_7/datetime/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace datetime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var dateTime = new DateTime(2015, 1, 1);
+
+            var start = DateTime.Now;
+            var end = start.AddMinutes(2);
+            var duration = end - start;
+            System.Console.WriteLine(duration);
+
+        }
+    }
+}
